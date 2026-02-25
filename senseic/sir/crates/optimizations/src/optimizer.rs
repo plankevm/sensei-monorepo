88e02a23e5914855231f8f71c485ccdbebd1ab52
Oana Barbu <akakob@gmail.com>
2026-02-25 14:13:10 +0000

    add tests

--- senseic/sir/crates/optimizations/src/optimizer.rs
+++ senseic/sir/crates/optimizations/src/optimizer.rs
@@ -75,3 +75,155 @@ impl Optimizer {
         std::mem::swap(&mut self.src, dst);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sir_analyses::legalize;
+    use sir_parser::{EmitConfig, parse_or_panic};
+    use sir_test_utils::assert_trim_strings_eq_with_diff;
+
+    fn optimize(source: &str, passes: &str) -> String {
+        let program = parse_or_panic(source, EmitConfig::init_only());
+        let mut optimizer = Optimizer::new(program);
+        optimizer.run_passes(passes);
+        let program = optimizer.finish();
+        legalize(&program).expect("optimized IR should be legal");
+        sir_data::display_program(&program)
+    }
+
+    const SWITCH_ON_COPY_WITH_DEAD_CODE: &str = r#"
+        fn init:
+            entry {
+                x = const 1
+                y = copy x
+                switch y {
+                    1 => @one
+                    default => @other
+                }
+            }
+            one {
+                dead = const 42
+                stop
+            }
+            other {
+                cond = const 0
+                => cond ? @other_yes : @one
+            }
+            other_yes { stop }
+    "#;
+
+    #[test]
+    fn test_csud() {
+        let expected = r#"
+Functions:
+    fn @0 -> entry @0  (outputs: 0)
+
+Basic Blocks:
+    @0 {
+        => @1
+    }
+
+    @1 {
+        stop
+    }
+        "#;
+
+        let actual = optimize(SWITCH_ON_COPY_WITH_DEAD_CODE, "csud");
+        assert_trim_strings_eq_with_diff(&actual, expected, "csud");
+    }
+
+    #[test]
+    fn test_cusd() {
+        let expected = r#"
+Functions:
+    fn @0 -> entry @0  (outputs: 0)
+
+Basic Blocks:
+    @0 {
+        $0 = const 0x1
+        => @1
+    }
+
+    @1 {
+        stop
+    }
+        "#;
+
+        let actual = optimize(SWITCH_ON_COPY_WITH_DEAD_CODE, "cusd");
+        assert_trim_strings_eq_with_diff(&actual, expected, "cusd");
+    }
+
+    #[test]
+    fn test_ucsd() {
+        let expected = r#"
+Functions:
+    fn @0 -> entry @0  (outputs: 0)
+
+Basic Blocks:
+    @0 {
+        $0 = const 0x1
+        $1 = copy $0
+        => @1
+    }
+
+    @1 {
+        stop
+    }
+        "#;
+
+        let actual = optimize(SWITCH_ON_COPY_WITH_DEAD_CODE, "ucsd");
+        assert_trim_strings_eq_with_diff(&actual, expected, "ucsd");
+    }
+
+    #[test]
+    fn test_uscd() {
+        let expected = r#"
+Functions:
+    fn @0 -> entry @0  (outputs: 0)
+
+Basic Blocks:
+    @0 {
+        $0 = const 0x1
+        $1 = copy $0
+        switch $0 {
+            1 => @1,
+            else => @2
+        }
+
+    }
+
+    @1 {
+        stop
+    }
+
+    @2 {
+        $2 = const 0x0
+        => @1
+    }
+        "#;
+
+        let actual = optimize(SWITCH_ON_COPY_WITH_DEAD_CODE, "uscd");
+        assert_trim_strings_eq_with_diff(&actual, expected, "uscd");
+    }
+
+    #[test]
+    fn test_scsud() {
+        let expected = r#"
+Functions:
+    fn @0 -> entry @0  (outputs: 0)
+
+Basic Blocks:
+    @0 {
+        => @1
+    }
+
+    @1 {
+        stop
+    }
+        "#;
+
+        let actual = optimize(SWITCH_ON_COPY_WITH_DEAD_CODE, "scsud");
+        assert_trim_strings_eq_with_diff(&actual, expected, "scsud");
+    }
+}
